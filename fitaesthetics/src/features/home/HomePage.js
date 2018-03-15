920a3b308ed903a457bff38f91931fc1b951e26c
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-03-15 20:44:04 +0530

    Initiated Navbar Component
    Added the navbar component and fixed alignment

--- fitaesthetics/src/features/home/HomePage.js
+++ fitaesthetics/src/features/home/HomePage.js
@@ -12,14 +12,33 @@ export class HomePage extends Component {
 
   render() {
     return (
-      <div className="home-home-page">
-       Hi rekit
-        <div class="alert alert-primary" role="alert">
-  This is a primary alert—check it out!
-</div>
 
+    <div id="container">
 
-      </div>
+         <nav class="navbar navbar-expand-lg navbar-dark" id="n-container">
+            <a class="navbar-brand mb-0 h1" href="#" id="txt-clr" >FitAestheticsLk</a>
+           <button className="navbar-toggler" type="button" id="txt-clr" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+             <span className="navbar-toggler-icon" />
+            </button>
+
+            <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
+           
+            <div class="navbar-nav" id="items">
+                <a class="nav-item nav-link active p-2" href="#" id="txt-clr">About<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link p-2" href="#" id="txt-clr">Features</a>
+                <a class="nav-item nav-link p-2" href="#" id="txt-clr">Sign up</a>
+                <a class="nav-item nav-link p-2" href="#" id="txt-clr">Log in</a>
+            </div>
+            
+            </div>
+        </nav>
+
+    </div>
+     
+
+   
+     
+
     );
   }
 }
