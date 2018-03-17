8e7620ff0d8a76b3016d213a943082cec6381f04
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-03-17 12:11:53 +0530

    Search Bar Fixed
    - Did proper search bar alignment
    - Also added multple cards in the featured section

--- fitaesthetics/src/features/home/HomePage.js
+++ fitaesthetics/src/features/home/HomePage.js
@@ -37,18 +37,21 @@ export class HomePage extends Component {
 
         
         <div class="jumbotron jumbotron-fluid" id="j-col">
+        
+        <div class="container" id="move">
 
-        <div class="container">
+        <div id="more">
+        
           <h1 class="display-4" id="txt-clr">FitAesthetics</h1>
-          <p class="lead" id="txt-clr"> <h8>We bring fitness to your fingertips. Find your personal trainer now.</h8></p>
+          <p class="lead" id="txt-clr">We bring fitness to your fingertips. Find your personal trainer now.</p>
           <hr class="my-4" />
        
           <form class="form-inline ">
-
-            <input class="form-control mr-sm-2 w-50 p-2" type="search" placeholder="Try Colombo" aria-label="Search" />
-            <button class="btn my-2 my-sm-0 p-2" type="submit" id="but-col">Search</button>
+          <input class="form-control mr-sm-2 w-75 p-4 " type="search" placeholder="Try Colombo" aria-label="Search" id="type" />
+          <button class="btn my-2 my-sm-0 p-3 " type="submit" id="but-col">Search</button>
           </form>
 
+       </div>
         </div>
 
         </div>
@@ -60,27 +63,68 @@ export class HomePage extends Component {
           <p class="lead text-center">Our highest rated trainers for the month</p>
 
        
-          <div class="card" id="crd">
-  <img class="card-img-top" src="..." alt="Card image cap" />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+          <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+     
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      
+    </div>
   </div>
 </div>
 
-   
-
-
-
-
-
+  
 
           </div>
 
+        </div>
 
+       
+        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel" data-interval="3000">
+        
+          <div className="carousel-inner">
+          
+
+          <div className="carousel-item active">
+          <img className="d-block w-100" src="/src/images/HomeImage.jpg" alt="First slide" />
+          </div>
+   
+         <div className="carousel-item">
+         <img className="d-block w-100" src="/src/images/pic1.png" alt="Second slide" />
+          </div>
+    
+         <div className="carousel-item">
+         <img className="d-block w-100" src="/src/images/pic3.jpg" alt="Third slide" />
+         </div>
+
+         
 
         </div>
+
+        </div>
+
+        
+
+
+
+
 
   
     
