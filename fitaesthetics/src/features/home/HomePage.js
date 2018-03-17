9bd2bee9ee444f32283f40541e6ac332ceec0a58
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-03-17 17:46:11 +0530

    Featured-container updates
    - Added content.
    - Added alignment.
    - Added images.
    - Added view profile button. 
    - View profile button alignment to be fixed.

--- fitaesthetics/src/features/home/HomePage.js
+++ fitaesthetics/src/features/home/HomePage.js
@@ -16,7 +16,7 @@ export class HomePage extends Component {
     <div id="container">
 
 
-        <nav class="navbar navbar-expand-lg navbar-dark" id="n-container">
+        <nav class="navbar navbar-expand-lg navbar-dark" id="nv-container">
           <a class="navbar-brand mb-0 h1" href="#" id="txt-clr">FitAestheticsLk</a>
           <button className="navbar-toggler" type="button" id="txt-clr" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
              <span className="navbar-toggler-icon" />
@@ -24,7 +24,7 @@ export class HomePage extends Component {
           
           <div class="collapse navbar-collapse flex-row-reverse" id="navbarNavAltMarkup">
            
-            <div class="navbar-nav" id="items">
+            <div class="navbar-nav">
                 <a class="nav-item nav-link active p-2" href="#" id="txt-clr">About<span class="sr-only">(current)</span></a>
                 <a class="nav-item nav-link p-2" href="#" id="txt-clr">Features</a>
                 <a class="nav-item nav-link p-2" href="#" id="txt-clr">Sign up</a>
@@ -36,65 +36,79 @@ export class HomePage extends Component {
         </nav>
 
         
-        <div class="jumbotron jumbotron-fluid" id="j-col">
+        <div class="jumbotron jumbotron-fluid" id="search-container">
         
-        <div class="container" id="move">
+          <div class="container" id="search-content">
 
-        <div id="more">
+            <div>
         
-          <h1 class="display-4" id="txt-clr">FitAesthetics</h1>
-          <p class="lead" id="txt-clr">We bring fitness to your fingertips. Find your personal trainer now.</p>
-          <hr class="my-4" />
+            <h1 class="display-4" id="txt-clr">FitAesthetics</h1>
+            <p class="lead" id="txt-clr">We bring fitness to your fingertips. Find your personal trainer now.</p>
+            <hr class="my-4" />
        
-          <form class="form-inline ">
-          <input class="form-control mr-sm-2 w-75 p-4 " type="search" placeholder="Try Colombo" aria-label="Search" id="type" />
-          <button class="btn my-2 my-sm-0 p-3 " type="submit" id="but-col">Search</button>
-          </form>
+            <form class="form-inline ">
+            <input class="form-control mr-sm-2 w-75 p-4 " type="search" placeholder="Try Colombo" aria-label="Search" id="search-input" />
+            <button class="btn my-2 my-sm-0 p-3 " type="submit" id="search-button">Search</button>
+            </form>
 
-       </div>
-        </div>
-
-        </div>
-
-        <div class="jumbotron jumbotron-fluid" id="bg-col">
-        
-          <div class="container">
-          <h1 class="display-4 text-center">Our Featured Trainers</h1>
-          <p class="lead text-center">Our highest rated trainers for the month</p>
-
-       
-          <div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap" />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-     
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap" />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap" />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      
-    </div>
-  </div>
-</div>
-
-  
-
+           </div>
           </div>
 
         </div>
+
+
+        <div class="jumbotron jumbotron-fluid" id="featured-container">
+        
+          <div class="container">
+          <h1 class="display-5 text-center">Our Featured Trainers</h1>
+          <p class="lead text-center">Pick from the highest rated trainers for this month</p>
+
+        <div class="my-sm-5">
+
+          <div class="card-deck">
+
+            <div class="card" id="border-color">
+            <img class="card-img-top w-100 p-3" src="/src/images/Profile-1.jpg" alt="Lazar Angelov" id="card-image"/>
+
+              <div class="card-body" id="move">
+              <h5 class="card-title text-center">Ulisess Jr</h5>
+              <p class="card-text">Ulisses Jr is one of the most sought after and highly respected bodybuilding and body composition coaches in the World.</p>
+             
+              <button type="button" class="btn btn-danger btn-lg my-sm-2 p-2 w-100">View Profile</button>
+        
+              </div>
+
+           </div>
+
+            <div class="card" id="border-color">
+            <img class="card-img-top w-100 p-3" src="/src/images/Profile-2.jpg" alt="Card image cap" id="card-image"/>
+
+              <div class="card-body">
+              <h5 class="card-title text-center">Simeon Panda</h5>
+              <p class="card-text">hetics® Sportswear and Just Lift.®, he is a fitness entrepreneur who is featured on Forbes as one of the worlds top ten influencers for fitness</p>
+              <button type="button" class="btn btn-danger btn-lg my-sm-2 p-2 w-100 ">View Profile</button>
+              </div>
+
+            </div>
+
+            <div class="card" id="border-color">
+            <img class="card-img-top w-100 p-3" src="/src/images/Profile-3.jpg" alt="Card image cap" id="card-image"/>
+
+              <div class="card-body">
+              <h5 class="card-title text-center">Lazar Angelov</h5>
+              <p class="card-text"> Lazar dominates other bodybuilders with balanced physique and incredible definition.As a personal trainer he has been able to transform the bodies of thousands of people and to make his motivation and knowledge contagious to them.</p>
+              <button type="button" class="btn btn-danger btn-lg my-sm-2 p-2 w-100 ">View Profile</button>
+              </div>
+
+            </div>
+
+        </div>
+
+      </div>
+
+      </div>
+
+      </div>
 
        
         <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel" data-interval="3000">
