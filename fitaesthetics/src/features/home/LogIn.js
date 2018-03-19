c2be1c252841b51cd6ff9eb3bff3b1c6914aa5b4
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-03-19 15:46:54 +0530

    Log In form - Completed
    - Completed the log in form

--- fitaesthetics/src/features/home/LogIn.js
+++ fitaesthetics/src/features/home/LogIn.js
@@ -19,18 +19,36 @@ export class LogIn extends Component {
 
       <div class="modal-content" id="mod1-settings">
 
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Log In</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
         <div class="d-flex justify-content-center">
 
         <div class="d-flex justify-content-center" id="mod-settings">
 
-      
-
         <form id="l-mod">
+
           <input type="email" class="form-control" id="input-sizing" placeholder="Email" />
-          <br />
-          <input type="password" class="form-control" id="input-sizing" placeholder="Password" />
-          <button type="button" class="btn btn-danger my-5" id="input-sizing">Log in</button>
+          <input type="password" class="form-control my-3" id="input-sizing" placeholder="Password" />
+          <button type="button" class="btn btn-danger my-4" id="input-sizing">Log in</button>
         </form>
+          <p class="text-center" >Forgot password?
+          <hr />
+          </p> 
+
+        <div class="d-flex justify-content-center">
+          <div class="d-flex flex-row">
+          <p>Don't have an account?</p>
+          <a class="ml-2">Sign Up</a>
+          </div>
+
+        </div>
+
+
         </div>
         
         </div>
