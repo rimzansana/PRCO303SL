05b4b3ad47286b50256170e137fc3057ac7d5bf1
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-03-29 22:01:39 +0530

    Home-Featured
    - Edited the position of the button.

--- fitaesthetics/src/features/home/HomeFeatured.js
+++ fitaesthetics/src/features/home/HomeFeatured.js
@@ -25,18 +25,22 @@ export class HomeFeatured extends Component {
         <div class="card mb-4 box-shadow">
          <div class="card-body">
 
-            <div>
+          
             <img class="rounded-circle" src="/src/images/Profile-1.jpg" alt="Image1" width="140" height="140" />
             <h2 class="my-4">Ulisess Jr</h2>
             <p>Ulisses Jr is one of the most sought after and highly respected bodybuilding and body composition coaches in the World.</p>
+          
+           
+         </div>
+
+            <div class="w-100 mb-5">
+            <button type="button" class="btn btn-lg  btn-outline-danger w-75">View Profile</button>
             </div>
 
 
-            <button type="button" class="btn btn-lg btn-block btn-outline-danger my-sm-5">View Profile</button>
-         </div>
         </div>
 
-                <div class="card mb-4 box-shadow">
+         <div class="card mb-4 box-shadow">
          <div class="card-body">
 
             <div>
@@ -44,10 +48,12 @@ export class HomeFeatured extends Component {
             <h2 class="my-4">Simeon Panda</h2>
             <p>hetics® Sportswear and Just Lift.®, he is a fitness entrepreneur who is featured on Forbes as one of the worlds top ten influencers for fitness</p>
             </div>
-
-
-            <button type="button" class="btn btn-lg btn-block btn-outline-danger my-sm-5">View Profile</button>
+     
          </div>
+
+             <div class="w-100 mb-5">
+            <button type="button" class="btn btn-lg  btn-outline-danger w-75">View Profile</button>
+            </div>
         </div>
 
                 <div class="card mb-4 box-shadow">
@@ -58,10 +64,11 @@ export class HomeFeatured extends Component {
             <h2 class="my-4">Lazar Angelov</h2>
             <p>As a personal trainer he has been able to transform the bodies of thousands of people and to make his motivation and knowledge contagious to them</p>
             </div>
-
-
-            <button type="button" class="btn btn-lg btn-block btn-outline-danger my-sm-5">View Profile</button>
          </div>
+
+               <div class="w-100 mb-5">
+            <button type="button" class="btn btn-lg  btn-outline-danger w-75">View Profile</button>
+            </div>
         </div>
 
 
