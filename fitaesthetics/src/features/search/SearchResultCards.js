4d2ee9e373e58ef02f86d8db42f2e203457e69e8
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-03-30 16:19:39 +0530

    Search-Page-Rating
    - Added columns.

--- fitaesthetics/src/features/search/SearchResultCards.js
+++ fitaesthetics/src/features/search/SearchResultCards.js
@@ -17,22 +17,43 @@ export class SearchResultCards extends Component {
        <div class="container">
          <div class="row mb-2">
         <div class="col-md-6">
+       
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
               <h3 class="mb-0">
                 <a class="text-dark">Lazar Angelov</a>
               </h3>
               <div class="text-muted">Body building , Calisthenics and cardio</div>
-              <p class="card-text mb-auto my-3">Hi, my name is Lazar Angelov. My passion for fitness is what led me to become a personal trainer.</p>
-              <p class="card-text mb-auto my-3 d-flex">Experience :<p> &nbsp; 5 Years</p></p>
-              <p class="card-text mb-auto d-flex">15000<p> &nbsp;LKR per month</p></p>
+              <p class="card-text my-4 d-flex">Experience :<p> &nbsp; 5 Years</p></p>
+              <p class="card-text mb-auto d-flex my-4">15000<p> &nbsp;LKR per month</p></p>
 
-              <a href="#">View Profile</a>
-            </div>
-            <img class="card-img-right flex-auto d-none d-md-block h-100" src="/src/images/card-profile.jpg" alt="Card image cap" width="160" />
+                
+                <div class="w-100">
+                <div class="row">
+                 <div class="col w-50">
+                <a href="#">View Profile</a>
+                    </div>
+                 <div class="col">
+                2 of 2
+                </div>
+                  </div>
+                  </div>
+               
+
+
+
+             
+
+
+
+              </div>
+
             
+            <img class="card-img-right flex-auto d-none d-md-block h-100" src="/src/images/card-profile.jpg" alt="Card image cap" width="160" />
+            </div>
           </div>
-        </div>
+      
+       
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
@@ -47,10 +68,12 @@ export class SearchResultCards extends Component {
             <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap" />
           </div>
         </div>
+
       </div>
     </div>
        
-      </div>
+   </div>
+
     );
   }
 }
