e5548c8619fd576601066b2f85bc23393e0567a1
Rimzan Sadikeen <rimzansadikeen@ymail.com>
2018-04-03 15:30:38 +0530

    Changed Search Banner Text color

--- fitaesthetics/src/features/search/SearchBanner.js
+++ fitaesthetics/src/features/search/SearchBanner.js
@@ -12,8 +12,8 @@ export default class SearchBanner extends Component {
           <div className="jumbotron jumbotron-fluid" id="search-container">
         
           <div className="container">
-            <h1 id="txt-clr">Find your perfect trainer below</h1>   
-             <p className="lead my-5" id="txt-clr">Browse through our professional trainers to find the best fit for you.</p>
+            <h1 className="txt-clr">Find your perfect trainer below</h1>   
+             <p className="lead my-5 txt-clr">Browse through our professional trainers to find the best fit for you.</p>
           </div>
 
         </div>
